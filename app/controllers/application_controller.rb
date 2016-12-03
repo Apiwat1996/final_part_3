f68ffc652652e414d47f5c4c2bde6c3c69a8a14c
Apiwat <57160552@go.buu.ac.th>
2016-12-03 15:30:17 +0700

    f1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,49 @@
+class ApplicationController < ActionController::Base
+  def hello
+	render html: "๑. แสงเทียน (Candlelight Blues)
+๒. ยามเย็น (Love At Sundown)
+๓. สายฝน (Falling Rain)
+๔. ใกล้รุ่ง (Near Dawn)
+๕. ชะตาชีวิต (H.M. Blues)
+๖. ดวงใจกับความรัก (Never Mind the H.M. Blues)
+๗. มาร์ชราชวัลลภ (Royal Guards March)
+๘. อาทิตย์อับแสง (Blue Day)
+๙. เทวาพาคู่ฝัน (Dream Of Love Dream of You)
+๑๐. คำหวาน (Sweet Words)
+๑๑. มหาจุฬาลงกรณ์
+๑๒. แก้วตาขวัญใจ (Lovelight in My Heart)
+๑๓. พรปีใหม่
+๑๔. รักคืนเรือน (Love Over Again)
+๑๕. ยามค่ำ (Twilight)
+๑๖. ยิ้มสู้ (Smiles)
+๑๗. มาร์ชธงไชยเฉลิมพล
+๑๘. เมื่อโสมส่อง (I Never Dream)
+๑๙. ลมหนาว (Love in Spring)
+๒๐. ศุกร์สัญลักษณ์ (Friday Night Rag)
+๒๑. Oh I say
+๒๒. Can't You Ever See
+๒๓. Lay Kram Goes Dixie
+๒๔. ค่ำแล้ว (Lullaby)
+๒๕. สายลม (I Think of You)
+๒๖. ไกลกังวล (When)
+๒๗. แสงเดือน (Magic Beams)
+๒๘. ฝัน (Somewhere Somehow)
+๒๙. มาร์ชราชนาวิกโยธิน
+๓๐. ภิรมย์รัก (A Love Story)
+๓๑. แผ่นดินของเรา (Alexandra)
+๓๒. พระมหามงคล
+๓๓. ยูงทอง
+๓๔. ในดวงใจนิรันดร์ (Still on My Mind)
+๓๕. เตือนใจ (Old Fashioned Melody)
+๓๖. ไร้เดือน (No Moon)
+๓๗. เกาะในฝัน (Dream Island)
+๓๘. แว่ว (ECHO)
+๓๙. เกษตรศาสตร์
+๔๐. ความฝันอังสูงสุด
+๔๑. เราสู้
+๔๒. เรา-เหล่าราบ ๒๑
+๔๓. BLUES FOR UTHIT
+๔๔. รัก
+๔๕. เมนูไข่"
+  end
+end
